c193ae7bfc44b10e0738381cd2cc08f8b9cf6553
Mehdi Oubraim <mehdioubraim3@gmail.com>
2022-05-07 00:50:59 +0000

    still fixing

--- src/main/java/com/mehdi/spring/controller/PersonRestController.java
+++ src/main/java/com/mehdi/spring/controller/PersonRestController.java
@@ -1,5 +1,6 @@
 package com.mehdi.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,12 @@ import com.mehdi.spring.model.Person;
 @RestController
 public class PersonRestController {
 
-	@RequestMapping("/rest")
+	@GetMapping("/rest")
 	public String healthCheck() {
 		return "OK";
 	}
 	
-	@RequestMapping("/rest/person/get")
+	@GetMapping("/rest/person/get")
 	public Person getPerson(@RequestParam(name = "name", required = false, defaultValue = "Unknown") String name) {
 		Person person = new Person();
 		person.setName(name);
